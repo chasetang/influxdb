e9ff46511ced6843aa920c078e1887fb5f16ee25
Jeff Wendling <jeff@influxdata.com>
2018-05-15 15:53:56 -0600

    attempt to work on docker on windows
    multiple users have attempted to run influxdb in a docker container
    with a windows host and a volume mounted from windows. that causes
    problems because it apparently uses samba/cifs which does not
    support fsync on directories. this patchset will, once, at startup,
    check if it can fsync "/". if it receives an EINVAL as is what
    appears to happen on samba/cifs, then it will not attempt to fsync
    any more. this should help.
    
    updates #9833.
    updates #9630.

--- pkg/file/file_unix.go
+++ pkg/file/file_unix.go
@@ -2,7 +2,10 @@
 
 package file
 
-import "os"
+import (
+	"os"
+	"syscall"
+)
 
 func SyncDir(dirName string) error {
 	// fsync the dir to flush the rename
@@ -11,7 +14,19 @@ func SyncDir(dirName string) error {
 		return err
 	}
 	defer dir.Close()
-	return dir.Sync()
+
+	// while we're on unix, we may be running in a docker container that is
+	// pointed at a windows volume over samba. that doesn't support fsyncs
+	// on directories. this shows itself as an EINVAL, so we ignore that
+	// error.
+	err = dir.Sync()
+	if pe, ok := err.(*os.PathError); ok && pe.Err == syscall.EINVAL {
+		err = nil
+	} else if err != nil {
+		return err
+	}
+
+	return dir.Close()
 }
 
 // RenameFile will rename the source to target using os function.
